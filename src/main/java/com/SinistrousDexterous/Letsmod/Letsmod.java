a3532eac25ad8afa740d953318ff44b25cfddbb3
SinistrousDexterous <sinistrousdexterous@gmail.com>
2014-12-11 19:06:59 +0800

    Episode 3 Phamiar

---
+++ src/main/java/com/SinistrousDexterous/Letsmod/Letsmod.java
@@ -0,0 +1,28 @@
+package com.SinistrousDexterous.Letsmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="Letsmod", name="Letsmod mod", version="1.7.2-0.1")
+public class Letsmod {
+   @Mod.Instance ("Letsmod")
+   public static Letsmod instance;
+
+   @Mod.EventHandler
+   public void Init(FMLPreInitializationEvent event)
+   {
+
+   }
+   @Mod.EventHandler
+   public void Init(FMLInitializationEvent event)
+   {
+
+   }
+   @Mod.EventHandler
+   public void Init(FMLPostInitializationEvent event)
+   {
+
+   }
+}
